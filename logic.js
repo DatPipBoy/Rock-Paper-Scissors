c7f14a99151c52d5e9e0c57621ac893a772404b2
Eball <116214829+DatPipBoy@users.noreply.github.com>
2025-08-31 17:04:49 -0400

    added computer logic

--- logic.js
+++ logic.js
@@ -1,1 +1,15 @@
-console.log("Hello world")
+//computer choice logic
+
+function getComputerChoice() {
+    const result = Math.random();
+
+    if (result <= 0.33) {
+        return "Rock";
+    } else if (result >0.33 && result <= 0.66) {
+        return "Paper"; 
+    } else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
